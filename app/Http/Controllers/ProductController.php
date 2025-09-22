eff137374535780c4e15e32d7ed36525d320f845
Paulasaah <paulasaavedra1101@gmail.com>
2025-09-21 20:42:15 -0500

    Controlador Basico

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function index(Request $request){
+        return "LISTADO DE PRODUCTOS";
+    }
+    function create(){
+        return "FORMULARIO DE PRODUCTOS";
+    }
+    function show($id, $category =  null)
+    {
+        if ($category == null) {
+            return "Detalle de cada producto: " . $id;
+        } else {
+            return "Detalle de cada producto:" . $id . "de la categoria". $category;
+        }
+    }
+}
