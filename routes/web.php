e34408152b6b4aa2690d97ccfe31248a230a96b3
Paulasaah <paulasaavedra1101@gmail.com>
2025-09-21 21:08:26 -0500

    Grupo de rutas basico

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('products') ->controller(ProductController::class)->group(function() {
+    Route::get('/','index' );
+
+    Route::get('/create', 'create' );
+
+    Route::get('/{id}/{category?}', 'show');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('products', [ProductController::class, 'index'] );
-
-Route::get('products/create', [ProductController::class,'create'] );
-
-Route::get('products/{id}/{category?}', [ProductController::class, 'show']);
