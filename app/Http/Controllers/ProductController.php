d625e818a5fb66d9142176eef82fa53976370679
Paulasaah <paulasaavedra1101@gmail.com>
2025-09-29 16:29:00 -0500

    Vistas basicas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,17 +7,13 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     function index(Request $request){
-        return "LISTADO DE PRODUCTOS";
+        return view('products.index');
     }
     function create(){
-        return "FORMULARIO DE PRODUCTOS";
+        return view('products.create');
     }
     function show($id, $category =  null)
     {
-        if ($category == null) {
-            return "Detalle de cada producto: " . $id;
-        } else {
-            return "Detalle de cada producto:" . $id . "de la categoria". $category;
-        }
+        return view('products.show');
     }
 }
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>FORMULARIO PARA CREAR UN NUEVO PRODUCTO</h1>
+    <form action="" method = "post">
+        <label for="nombre">Name:</label>
+        <input type="text" name="nombre">
+        <br>
+        <label for="description">Description:</label>
+        <textarea name="descrption" cols="30" rows="10">
+        </textarea>
+        <br>
+        <label for="price">Price:</label>
+        <input type="number" name="precio">
+        <br>
+        <label for="image">Image:</label>
+        <input type="file" name="img">
+        <br>
+        <label for="brand">Brand:</label>
+        <input type="text" name="brand">
+    </form>
+</body>
+</html>
